0507faab76cc4109fd2bda9b7e71fbaff9044e26
UnaiPerez2DAW <uperezchic@educacion.navarra.es>
2021-11-16 10:58:49 +0100

    Unai:Errores solucionados

--- Empleado.php
+++ Empleado.php
@@ -84,4 +84,72 @@
             return "Emplead@: " . $this->nombre . " " . $this->apellidos . " con Nº Social " . $this->numeroSeguridadSocial;
         }
     }
+
+    Class EmpleadoPlantilla extends Empleado{
+        
+        private $sueldo;
+        private $dietas;
+
+        Public function __construct($nombre,$apellidos,$numSoc,$sueldo,$dietas)
+        {
+            parent::__construct($nombre,$apellidos,$numSoc);
+            $this->sueldo = $sueldo;
+            $this->dietas = $dietas;
+        }
+
+        /**
+         * Get the value of sueldo
+         */ 
+        public function getSueldo()
+        {
+                return $this->sueldo;
+        }
+
+        /**
+         * Set the value of sueldo
+         *
+         * @return  self
+         */ 
+        public function setSueldo($sueldo)
+        {
+                $this->sueldo = $sueldo;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of dietas
+         */ 
+        public function getDietas()
+        {
+                return $this->dietas;
+        }
+
+        /**
+         * Set the value of dietas
+         *
+         * @return  self
+         */ 
+        public function setDietas($dietas)
+        {
+                $this->dietas = $dietas;
+
+                return $this;
+        }
+
+        /**
+         * @return Devuelve los ingresos mensuales
+         */
+        public function ingresos(){
+            return $this->sueldo + $this->dietas;
+        }
+
+        /**
+         * @return Cadena con información
+        */
+        Public function mostrar(){
+            return parent::mostrarEmpleado() . ", ingresa " . $this->ingresos() . "€. ";
+        }
+    
+    }
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
+    include "Empleado.php";
     include "EmpleadoPlantilla.php";
     $resultado = "";
 
-    $Emp1 = new EmpleadoPlantilla("Unai","Pérez","31-55555555-69",800,600);
-    $Emp2 = new EmpleadoPlantilla("Enrique","Lafraya","31-66666666-69",1300,900);
-    $resultado .= $Emp1->mostrar() . "<br>";
-    $resultado .= $Emp2->mostrar();
+    $Emp1 = new Empleado("Unai","Pérez","31-55555555-69");
+    $Emp2 = new Empleado("Enrique","Lafraya","31-66666666-69");
+    $resultado.= "<h2>Empleado</h2>";
+    $resultado .= $Emp1->mostrarEmpleado() . "<br>";
+    $resultado .= $Emp2->mostrarEmpleado();
+
+    $EmpPlantilla1 = new EmpleadoPlantilla("Unai","Pérez","31-55555555-69",800,600);
+    $EmpPlantilla2 = new EmpleadoPlantilla("Enrique","Lafraya","31-66666666-69",1300,900);
+    $resultado.= "<h2>Empleado Plantilla</h2>";
+    $resultado .= $EmpPlantilla1->mostrar() . "<br>";
+    $resultado .= $EmpPlantilla2->mostrar();
+
     include "vistas/vista_resultado.php";
 ?>
